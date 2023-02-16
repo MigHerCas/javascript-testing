15422d9168e3c5e66de0d83902354e73c27eb0f1
Miguel Hernanz <migherdcas@gmail.com>
2023-02-16 13:27:55 +0800

    base set-up

--- src/testing-fundamentals.ts
+++ src/testing-fundamentals.ts
@@ -19,3 +19,7 @@ function expect(actual: unknown) {
 let result = sum(3, 7);
 let expected = 10;
 expect(result).toBe(expected);
+
+result = subtract(7, 3);
+expected = 4;
+expect(result).toBe(expected);
