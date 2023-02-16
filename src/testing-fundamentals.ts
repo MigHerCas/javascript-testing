58a50440e943c44a0a91ff0b89d28d226d453d18
Miguel Hernanz <migherdcas@gmail.com>
2023-02-16 13:10:52 +0800

    testing fundamental idea

---
+++ src/testing-fundamentals.ts
@@ -0,0 +1,21 @@
+function sum(a: number, b: number) {
+  return a + b;
+}
+
+function subtract(a: number, b: number) {
+  return a - b;
+}
+
+function expect(actual: unknown) {
+  return {
+    toBe(expected: unknown) {
+      if (actual !== expected) {
+        throw new Error(`${actual} is not equal to ${expected}`);
+      }
+    },
+  };
+}
+
+let result = sum(3, 7);
+let expected = 10;
+expect(result).toBe(expected);
